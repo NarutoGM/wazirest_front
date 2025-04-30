8125a1aeea1c0385c408812f44763e2f1443cd20
NarutoGM <t1053300121@unitru.edu.pe>
2025-04-30 16:57:45 -0500

    fix(): fix error in production

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
+
+
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "lh3.googleusercontent.com",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
